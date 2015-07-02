f08a55e7a6203833ca9a778bb36026c4b16fb028
diadatp <admin@diadatp.com>
2015-07-02 19:28:52 +0530

    Testing Travis CI: fixing the build

--- Test.php
+++ Test.php
@@ -1,10 +1,9 @@
 <?php
 
-class Test extends PHPUnit_Framework_TestCase
-{
+class Test extends PHPUnit_Framework_TestCase {
 	public function testOnePlusOne() {
-		$this->assertEquals(1+1,1);
-  	}
+		$this->assertEquals(1+1, 2);
+	}
 }
 
 ?>
